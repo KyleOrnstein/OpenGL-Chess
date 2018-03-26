d4970512dde1bb8fbc01f81451e395a8f028ab28
kyle ornstein <bkao@cox.net>
2018-03-26 13:28:03 -0400

    Initial Commit

---
+++ Graphics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphics
+//
+//  Created by Kyle Ornstein on 3/26/18.
+//  Copyright © 2018 Kyle Ornstein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
